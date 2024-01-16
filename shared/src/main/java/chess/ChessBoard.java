056418fe4c7703842fe94f61febb4095251852f3
Ryan Callahan <54230450+Ryan-Callahan@users.noreply.github.com>
2024-01-16 16:44:32 -0700

    ChessBoard.getPiece implemented

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -30,7 +30,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return this.pieces[position.getRow()][position.getColumn()];
     }
 
     /**
