45c932da5bed1be0c76f7e3ac4947f470f6bcd9f
ryanc <ryan.callahan1124@gmail.com>
2024-02-27 16:57:44 -0700

    Started to write the clear() test

--- server/src/test/java/serviceTests/ServiceTests.java
+++ server/src/test/java/serviceTests/ServiceTests.java
@@ -1,5 +1,19 @@
 package serviceTests;
 
+import dataAccess.GameDAO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import passoffTests.testClasses.TestException;
+import service.GameService;
+
 //TODO implement
 public class ServiceTests {
+
+    @Test
+    public void clearService() throws TestException {
+        GameService game = new GameService();
+        game.clear();
+
+        Assertions.assertNull(GameData);
+    }
 }
