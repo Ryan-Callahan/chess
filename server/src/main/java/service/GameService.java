29f3ea590121644385d350e815c875355e86319a
ryanc <ryan.callahan1124@gmail.com>
2024-02-27 17:02:16 -0700

    Created endpoints for GameServices and UserServices

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,5 +1,12 @@
 package service;
 
+import model.AuthData;
+import model.GameData;
+
 //TODO implement
 public class GameService {
+    public GameData listGames(AuthData auth) {}
+    public GameData createGame(AuthData auth, GameData game) {}
+    public GameData joinGame(AuthData auth, GameData game) {}
+    public void clear() {}
 }
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,5 +1,12 @@
 package service;
 
+import model.AuthData;
+import model.UserData;
+
 //TODO implement
 public class UserService {
+    public AuthData register(UserData user) {}
+    public AuthData login(UserData user) {}
+    public void logout(UserData user, AuthData auth) {}
+
 }
