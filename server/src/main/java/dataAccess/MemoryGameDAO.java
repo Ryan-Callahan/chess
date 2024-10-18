9035fde6a863ab46dfab02cfc21fff460d135336
Ryan Callahan <ryan.callahan1124@gmail.com>
2024-10-18 15:03:06 -0600

    implemented MemoryGameDAO and MemoryUserDAO

--- server/src/main/java/dataAccess/MemoryGameDAO.java
+++ server/src/main/java/dataAccess/MemoryGameDAO.java
@@ -1,4 +1,52 @@
 package dataAccess;
 
-public class MemoryGameDAO {
+import dataAccess.interfaces.GameDAO;
+import model.GameData;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+public class MemoryGameDAO implements GameDAO {
+    private HashMap<Integer, GameData> gameTable = new HashMap<>();
+    @Override
+    public void createGame(GameData game) throws DataAccessException {
+        if (!gameTable.containsKey(game.gameID())) {
+            gameTable.put(game.gameID(), game);
+        } else {
+            throw new DataAccessException("Invalid GameID; Game already exists!");
+        }
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        if (gameTable.containsKey(gameID)) {
+            return gameTable.get(gameID);
+        } else {
+            throw new DataAccessException("Invalid GameID; Could not find game!");
+        }
+    }
+
+    @Override
+    public Collection<GameData> listGames() {
+        return gameTable.values();
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+        if (gameTable.containsKey(game.gameID())) {
+            gameTable.put(game.gameID(), game);
+        } else {
+            throw new DataAccessException("Invalid Game update; Game does not exits!");
+        }
+    }
+
+    @Override
+    public void deleteGame(GameData game) {
+        gameTable.remove(game.gameID());
+    }
+
+    @Override
+    public void clear() {
+        gameTable = new HashMap<>();
+    }
 }
--- server/src/main/java/dataAccess/MemoryUserDAO.java
+++ server/src/main/java/dataAccess/MemoryUserDAO.java
@@ -1,4 +1,46 @@
 package dataAccess;
 
-public class MemoryUserDAO {
+import dataAccess.interfaces.UserDAO;
+import model.UserData;
+
+import java.util.HashMap;
+
+public class MemoryUserDAO implements UserDAO {
+    HashMap<String, UserData> userTable = new HashMap<>();
+    @Override
+    public void createUser(UserData user) throws DataAccessException{
+        if (!userTable.containsKey(user.username())) {
+            userTable.put(user.username(), user);
+        } else {
+            throw new DataAccessException("Invalid username; Username already exists!");
+        }
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException{
+        if (userTable.containsKey(username)) {
+            return userTable.get(username);
+        } else {
+            throw new DataAccessException("Invalid Username; Could not find user!");
+        }
+    }
+
+    @Override
+    public void updateUser(UserData user) throws DataAccessException{
+        if (userTable.containsKey(user.username())) {
+            userTable.put(user.username(), user);
+        } else {
+            throw new DataAccessException("Invalid User Update; User does not exist!");
+        }
+    }
+
+    @Override
+    public void deleteUser(UserData user) {
+        userTable.remove(user.username());
+    }
+
+    @Override
+    public void clear() {
+        userTable = new HashMap<>();
+    }
 }
