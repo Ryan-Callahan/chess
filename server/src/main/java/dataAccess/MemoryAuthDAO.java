8e5b570c8c9064b2a70276baeb98be11c7b615a5
Ryan Callahan <ryan.callahan1124@gmail.com>
2024-10-17 20:53:56 -0600

    added data access classes and interfaces, updated package name

---
+++ server/src/main/java/dataAccess/MemoryAuthDAO.java
@@ -0,0 +1,16 @@
+package dataAccess;
+
+public class MemoryAuthDAO {
+//    Here are some examples of the kinds of methods your DAOs will need to support. This list is not exhaustive. You should consult your server design in order to determine all of the methods you need to provide.
+//
+    //    clear: A method for clearing all data from the database. This is used during testing.
+    //    createUser: Create a new user.
+    //    getUser: Retrieve a user with the given username.
+    //    createGame: Create a new game.
+    //    getGame: Retrieve a specified game with the given game ID.
+    //    listGames: Retrieve all games.
+    //    updateGame: Updates a chess game. It should replace the chess game string corresponding to a given gameID. This is used when players join a game or when a move is made.
+    //    createAuth: Create a new authorization.
+    //    getAuth: Retrieve an authorization given an authToken.
+    //    deleteAuth: Delete an authorization so that it is no longer valid.
+}
