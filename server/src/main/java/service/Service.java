73d22862572a933ccd41cc4e93e4c13c6b7b7ebd
Ryan Callahan <ryan.callahan1124@gmail.com>
2024-10-18 17:13:31 -0600

    added clearAllDB to Service.java

--- server/src/main/java/service/Service.java
+++ server/src/main/java/service/Service.java
@@ -6,9 +6,22 @@ import dataAccess.MemoryUserDAO;
 import dataAccess.interfaces.AuthDAO;
 import dataAccess.interfaces.GameDAO;
 import dataAccess.interfaces.UserDAO;
+import model.result.EmptyResult;
+import model.result.ErrorResult;
 
 public interface Service {
     GameDAO gameDAO = new MemoryGameDAO();
     UserDAO userDAO = new MemoryUserDAO();
     AuthDAO authDAO = new MemoryAuthDAO();
+
+    default Object clearAllDB() {
+        try {
+            gameDAO.clear();
+            userDAO.clear();
+            authDAO.clear();
+            return new EmptyResult(200);
+        } catch (Exception e) {
+            return new ErrorResult(500, e.getMessage());
+        }
+    }
 }
