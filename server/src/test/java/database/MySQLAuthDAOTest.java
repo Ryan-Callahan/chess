c7fb3066b6041e11de25efec3b6131107697f971
Ryan Callahan <ryan.callahan1124@gmail.com>
2024-11-01 15:38:23 -0600

    wrote tests for authDao

--- server/src/test/java/database/MySQLAuthDAOTest.java
+++ server/src/test/java/database/MySQLAuthDAOTest.java
@@ -3,21 +3,19 @@ package database;
 import dataaccess.DataAccessException;
 import dataaccess.mysqldao.MySQLAuthDAO;
 import model.AuthData;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class MySQLAuthDAOTest {
-    static MySQLAuthDAO testAuthDao = new MySQLAuthDAO();
-    static final String testAuthToken = "testToken";
+    MySQLAuthDAO testAuthDao = new MySQLAuthDAO();
+    final String testAuthToken = "testToken";
     final String testAuthToken2 = "testToken2";
-    static final String testUsername = "username";
+    final String testUsername = "username";
     final String testUsername2 = "username2";
-    static final AuthData testAuth = new AuthData(testAuthToken, testUsername);
+    final AuthData testAuth = new AuthData(testAuthToken, testUsername);
     final AuthData testAuth2 = new AuthData(testAuthToken2, testUsername2);
 
-    @BeforeAll
-    static void setup() throws DataAccessException {
+    @BeforeEach
+    void setup() throws DataAccessException {
         testAuthDao.clear();
         testAuthDao.createAuth(testAuth);
     }
@@ -26,30 +24,46 @@ public class MySQLAuthDAOTest {
     @DisplayName("Should create an auth correctly")
     void createAuthTest() throws DataAccessException {
         testAuthDao.createAuth(testAuth2);
-
+        var actual = testAuthDao.getAuthByToken(testAuthToken2);
+        Assertions.assertEquals(testAuth2, actual);
     }
 
     @Test
     @DisplayName("Should fail to create an auth when the auth token already exists")
-    void createFailTest() {}
+    void createFailTest() {
+        Assertions.assertThrows(DataAccessException.class, () -> testAuthDao.createAuth(testAuth));
+    }
 
     @Test
     @DisplayName("Should retrieve the correct auth")
-    void getAuthTest() {}
+    void getAuthTest() throws DataAccessException {
+        var actual = testAuthDao.getAuthByToken(testAuthToken);
+        Assertions.assertEquals(testAuth, actual);
+    }
 
     @Test
     @DisplayName("Should fail to get auth when bad authToken")
-    void getBadAuthTokenTest() {}
+    void getBadAuthTokenTest() {
+        Assertions.assertThrows(DataAccessException.class, () -> testAuthDao.getAuthByToken(testAuthToken2));
+    }
 
     @Test
     @DisplayName("Should remove auth")
-    void removeAuthTest() {}
+    void removeAuthTest() throws DataAccessException {
+        testAuthDao.removeAuth(testAuthToken);
+        Assertions.assertThrows(DataAccessException.class, () -> testAuthDao.getAuthByToken(testAuthToken));
+    }
 
     @Test
     @DisplayName("Should fail to remove auth")
-    void failRemoveAuthTest() {}
+    void failRemoveAuthTest() {
+        Assertions.assertThrows(DataAccessException.class, () -> testAuthDao.removeAuth(testAuthToken2));
+    }
 
     @Test
     @DisplayName("Should clear the auth db")
-    void clearAuthTest() {}
+    void clearAuthTest() throws DataAccessException {
+        testAuthDao.clear();
+        Assertions.assertThrows(DataAccessException.class, () -> testAuthDao.getAuthByToken(testAuthToken));
+    }
 }
