8226da71deb257ae8e5536989348a6f1da57b7dd
Ryan Callahan <ryan.callahan1124@gmail.com>
2024-11-13 15:22:03 -0700

    added logout to PostloginClient

--- client/src/main/java/ui/PostloginClient.java
+++ client/src/main/java/ui/PostloginClient.java
@@ -1,8 +1,15 @@
 package ui;
 
-public class PostloginClient implements Client {
-    public PostloginClient(String serverUrl) {
+import server.ServerFacade;
 
+import java.util.Arrays;
+
+import static ui.ClientType.LOGGED_IN;
+
+public class PostloginClient extends Client {
+    public PostloginClient(String serverUrl) {
+        server = new ServerFacade(serverUrl);
+        currentClient = LOGGED_IN;
     }
     @Override
     public String help() {
@@ -18,6 +25,41 @@ public class PostloginClient implements Client {
 
     @Override
     public String eval(String input) {
+        try {
+            String[] inputArray = input.toLowerCase().split(" ");
+            String command = inputArray[0];
+            String[] params = Arrays.copyOfRange(inputArray, 1, inputArray.length);
+            return switch (command) {
+                case "logout" -> logout();
+                case "createGame" -> createGame(params);
+                case "listGames" -> listGames();
+                case "joinGame" -> joinGame(params);
+                case "observeGame" -> observeGame(params);
+                default -> response(help());
+            };
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    private String logout() {
+        demoteClient();
+        return response("Logging out...");
+    }
+
+    private String createGame(String... params) {
+        return null;
+    }
+
+    private String listGames(String... params) {
+        return null;
+    }
+
+    private String joinGame(String... params) {
+        return null;
+    }
+
+    private String observeGame(String... params) {
         return null;
     }
 }
