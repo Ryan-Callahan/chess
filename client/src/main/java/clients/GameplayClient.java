efe4e64da8c11974a65d5869e2b1b744cab35a9f
Ryan Callahan <ryan.callahan1124@gmail.com>
2024-12-06 01:26:43 -0700

    re-added some old code to GameplayClient

--- client/src/main/java/clients/GameplayClient.java
+++ client/src/main/java/clients/GameplayClient.java
@@ -1,5 +1,8 @@
 package clients;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 import static clients.ClientType.IN_GAME;
 
 public class GameplayClient extends Client {
@@ -10,11 +13,49 @@ public class GameplayClient extends Client {
 
     @Override
     public String help() {
-        return null;
+        return """
+                - redraw_board
+                - highlight_legal_moves
+                - make_move
+                - leave
+                - resign
+                - help
+                """;
     }
 
     @Override
     public String eval(String input) {
-        return null;
+        try {
+            String[] inputArray = input.toLowerCase().split(" ");
+            String command = inputArray[0];
+            String[] params = Arrays.copyOfRange(inputArray, 1, inputArray.length);
+            return switch (command) {
+                case "redraw_board" -> redrawBoard();
+                case "leave" -> leave();
+                case "make_move" -> makeMove(params);
+                case "resign" -> resign();
+                case "highlight_legal_moves" -> highlightLegalMoves(params);
+                default -> response(help());
+            };
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    private String redrawBoard() {
+    }
+
+    private String leave() throws IOException {
+
+    }
+
+    private String makeMove(String... params) throws IOException {
+    }
+
+    private String resign(String... params) throws IOException {
+    }
+
+    private String highlightLegalMoves(String... params) throws IOException {
+
     }
 }
