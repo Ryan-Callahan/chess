99e0a7f259ce7b935403506c4f43bf73095b2dfa
Ryan Callahan <ryan.callahan1124@gmail.com>
2024-12-06 00:12:00 -0700

    Added structure to WebSocketHandler

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,4 +1,45 @@
 package server.websocket;
 
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import serializer.GSerializer;
+import websocket.commands.MakeMoveCommand;
+import websocket.commands.UserGameCommand;
+
+import javax.websocket.Session;
+import java.io.IOException;
+
+@WebSocket
 public class WebSocketHandler {
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException {
+        UserGameCommand command = GSerializer.deserialize(message, UserGameCommand.class);
+        switch (command.getCommandType()) {
+            case LEAVE -> leave(command);
+            case RESIGN -> resign(command);
+            case CONNECT -> connect(command);
+            case MAKE_MOVE -> {
+                MakeMoveCommand makeMoveCommand = GSerializer.deserialize(message, MakeMoveCommand.class);
+                makeMove(makeMoveCommand);
+            }
+        }
+    }
+
+    private void connect(UserGameCommand command) {
+
+    }
+
+    private void makeMove(MakeMoveCommand command) {
+
+    }
+
+    private void resign(UserGameCommand command) {
+
+    }
+
+    private void leave(UserGameCommand command) {
+
+    }
 }
