852d45bcdaf6497b400db54a3cd052053386718d
Ryan Callahan <ryan.callahan1124@gmail.com>
2024-12-06 00:01:52 -0700

    Added NotificationType to NotificationMessage class

--- shared/src/main/java/websocket/messages/NotificationMessage.java
+++ shared/src/main/java/websocket/messages/NotificationMessage.java
@@ -5,12 +5,29 @@ import static websocket.messages.ServerMessage.ServerMessageType.NOTIFICATION;
 public class NotificationMessage extends ServerMessage {
     private final String message;
 
-    public NotificationMessage(String message) {
+    private final NotificationType type;
+
+    public enum NotificationType {
+        CONNECTION,
+        OBSERVATION,
+        MOVE,
+        LEAVE,
+        RESIGN,
+        CHECK,
+        CHECKMATE
+    }
+
+    public NotificationMessage(NotificationType type, String message) {
         super(NOTIFICATION);
         this.message = message;
+        this.type = type;
     }
 
     public String getMessage() {
         return this.message;
+    }
+
+    public NotificationType getType() {
+        return this.type;
     }
 }
