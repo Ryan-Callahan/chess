7e08d74fd3dc2b8fdae437ff80b1b62b2c796b6c
Ryan Callahan <ryan.callahan1124@gmail.com>
2024-12-06 11:49:24 -0700

    fixed a bug

--- shared/src/main/java/websocket/messages/LoadGameMessage.java
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -5,14 +5,14 @@ import model.GameData;
 import static websocket.messages.ServerMessage.ServerMessageType.LOAD_GAME;
 
 public class LoadGameMessage extends ServerMessage {
-    private final GameData gameData;
+    private final GameData game;
 
     public LoadGameMessage(GameData gameData) {
         super(LOAD_GAME);
-        this.gameData = gameData;
+        this.game = gameData;
     }
 
-    public GameData getGameData() {
-        return this.gameData;
+    public GameData getGame() {
+        return this.game;
     }
 }
